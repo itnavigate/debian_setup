0cda56d40c810b1bd614fdefac39ff357ee8053a
Colin Manning <Colin@DearestEmmesArt.com.au>
2024-07-12 23:59:25 +0000

    added docker_compose_setup

---
+++ docker_compose_setup.sh
@@ -0,0 +1,27 @@
+sudo apt update
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+lsb_release_is=`lsb_release -is | tr '[:upper:]' '[:lower:]'`
+curl -fsSL https://download.docker.com/linux/$lsb_release_is/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$lsb_release_is $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+# apt-cache policy docker-ce
+
+sudo apt install -y docker-ce
+
+sudo systemctl status docker
+
+sudo usermod -aG docker ${USER}
+
+mkdir -p ~/.docker/cli-plugins/
+
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+docker compose version
+#Now logout, then back in to run without sudo
